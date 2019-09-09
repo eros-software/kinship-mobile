32f5dfb5a8bdc91da949712a4f965344f02ed76c
thoffart <thiagohoffart321920@gmail.com>
2019-09-09 11:13:57 -0300

    feat(shared-preferences): Adicionando InitialSetupWidgetState ao State da aplicacao

--- lib/app-middleware.dart
+++ lib/app-middleware.dart
@@ -1,9 +1,11 @@
 import 'package:kinship_mobile/app-state.dart';
 import 'package:kinship_mobile/pages/home-page/+state/home-page-middleware.dart';
+import 'package:kinship_mobile/pages/initial-setup-widget/+state/initial-setup-widget-middleware.dart';
 import 'package:kinship_mobile/pages/login-page/+state/login-page-middleware.dart';
 import 'package:redux/redux.dart';
 
 final List<Middleware<AppState>> appMiddleware = [
   LoginPageMiddleware(),
   HomePageMiddleware(),
+  InitialSetupWidgetMiddleware()
 ];
--- lib/app-reducers.dart
+++ lib/app-reducers.dart
@@ -1,8 +1,10 @@
 import 'package:kinship_mobile/app-state.dart';
 import 'package:kinship_mobile/pages/home-page/+state/home-page-reducers.dart';
+import 'package:kinship_mobile/pages/initial-setup-widget/+state/initial-setup-widget-reducers.dart';
 import 'package:kinship_mobile/pages/login-page/+state/login-page-reducers.dart';
 
 AppState appReducers(AppState state, dynamic action) => new AppState(
   loginPageState: loginPageReducer(state.loginPageState, action),
   homePageState: homePageReducer(state.homePageState, action),
+  initialSetupWidgetState: initialSetupWidgetReducers(state.initialSetupWidgetState, action),
 );
