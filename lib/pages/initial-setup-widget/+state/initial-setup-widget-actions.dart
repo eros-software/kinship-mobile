9faacbe5b60a849cb32674cf2c4c3e7f14f9811b
thoffart <thiagohoffart321920@gmail.com>
2019-09-09 11:12:01 -0300

    feat(shared-preferences): Criando InitialSetupWidget e seu State

---
+++ lib/pages/initial-setup-widget/+state/initial-setup-widget-actions.dart
@@ -0,0 +1,10 @@
+import 'package:kinship_mobile/models/usuario.dart';
+
+class VerifyPreferences { }
+
+class DeletePreferences { }
+
+class SavePreferences {
+  final Usuario user;
+  SavePreferences(this.user);
+}
