dc67e2d45b92a5f73b62098d6eebe8d6448a12d5
thoffart <thiagohoffart321920@gmail.com>
2019-11-06 11:36:31 -0300

    feat(posts): adicionando states criados ao app

--- lib/app-reducers.dart
+++ lib/app-reducers.dart
@@ -1,5 +1,7 @@
 import 'package:kinship_mobile/app-state.dart';
 import 'package:kinship_mobile/pages/home-page/+state/home-page-reducers.dart';
+import 'package:kinship_mobile/pages/home-page/posts-list-page/+state/posts-list-page-reducers.dart';
+import 'package:kinship_mobile/pages/home-page/posts-list-page/create-post-page/+state/create-post-page-reducers.dart';
 import 'package:kinship_mobile/pages/initial-setup-widget/+state/initial-setup-widget-reducers.dart';
 import 'package:kinship_mobile/pages/login-page/+state/login-page-reducers.dart';
 
@@ -7,4 +9,6 @@ AppState appReducers(AppState state, dynamic action) => new AppState(
   loginPageState: loginPageReducer(state.loginPageState, action),
   homePageState: homePageReducer(state.homePageState, action),
   initialSetupWidgetState: initialSetupWidgetReducers(state.initialSetupWidgetState, action),
+  createPostPageState: createPostPageReducers(state.createPostPageState, action),
+  postsListPageState: postsListPageReducers(state.postsListPageState, action),
 );
--- lib/app-state.dart
+++ lib/app-state.dart
@@ -1,4 +1,6 @@
 import 'package:kinship_mobile/pages/home-page/+state/home-page-state.dart';
+import 'package:kinship_mobile/pages/home-page/posts-list-page/+state/posts-list-page-state.dart';
+import 'package:kinship_mobile/pages/home-page/posts-list-page/create-post-page/+state/create-post-page-state.dart';
 import 'package:kinship_mobile/pages/initial-setup-widget/+state/initial-setup-widget-state.dart';
 import 'package:kinship_mobile/pages/login-page/+state/login-page-state.dart';
 import 'package:meta/meta.dart';
@@ -8,11 +10,15 @@ class AppState {
   final LoginPageState loginPageState;
   final HomePageState homePageState;
   final InitialSetupWidgetState initialSetupWidgetState;
+  final CreatePostPageState createPostPageState;
+  final PostsListPageState postsListPageState;
 
   AppState({
     @required this.loginPageState,
     @required this.homePageState,
     @required this.initialSetupWidgetState,
+    @required this.createPostPageState,
+    @required this.postsListPageState,
   });
 
   factory AppState.initial() {
@@ -20,6 +26,8 @@ class AppState {
       loginPageState: LoginPageState.initial(),
       homePageState: HomePageState.initial(),
       initialSetupWidgetState: InitialSetupWidgetState.initial(),
+      createPostPageState: CreatePostPageState.initial(),
+      postsListPageState: PostsListPageState.initial(),
     );
   }
 
@@ -27,11 +35,15 @@ class AppState {
     LoginPageState loginPageState,
     HomePageState homePageState,
     InitialSetupWidgetState initialSetupWidgetState,
+    CreatePostPageState createPostPageState,
+    PostsListPageState postsListPageState,
   }) {
     return AppState(
       loginPageState: loginPageState ?? this.loginPageState,
       homePageState: homePageState ?? this.homePageState,
       initialSetupWidgetState: initialSetupWidgetState ?? this.initialSetupWidgetState,
+      createPostPageState: createPostPageState ?? this.createPostPageState,
+      postsListPageState: postsListPageState ?? this.postsListPageState,
     );
   }
 }
